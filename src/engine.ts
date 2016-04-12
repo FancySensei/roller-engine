46deb420c548a9c6c0c3e5257bbc0e92df05c6cc
Chuan Liu <chuanliu87@gmail.com>
2016-04-12 02:15:47 +0100

    Changed the code format.

--- src/engine.ts
+++ src/engine.ts
@@ -2,42 +2,37 @@
 import Container = PIXI.Container;
 import Sprite = PIXI.Sprite;
 
-class Engine
-{
-	private renderer:PIXI.CanvasRenderer | PIXI.WebGLRenderer;
-	private stage:Container;
-	private candy:Sprite;
-	
-	constructor(width:number = 800, height:number = 600)
-	{
+class Engine {
+	private renderer: PIXI.CanvasRenderer | PIXI.WebGLRenderer;
+	private stage: Container;
+	private candy: Sprite;
+
+	constructor(width: number = 800, height: number = 600) {
 		this.renderer = PIXI.autoDetectRenderer(width, height);
 		document.body.appendChild(this.renderer.view);
-		
+
 		this.stage = new Container();
-		
+
 		this.candy = Sprite.fromImage("./assets/candy.png", false);
 		this.candy.anchor.set(0.5, 0.5);
 		this.candy.position.set(400, 300);
-		
+
 		this.stage.addChild(this.candy);
-		
+
 		this.onFrameRequestCallback(0);
 	}
-	
-	private update():void
-	{
+
+	private update(): void {
 		this.candy.rotation += 0.1;
 	}
-	
-	private render():void
-	{
+
+	private render(): void {
 		this.renderer.render(this.stage);
 	}
-	
-	private onFrameRequestCallback = (timeStamp:number):void =>
-	{
+
+	private onFrameRequestCallback = (timeStamp: number): void => {
 		requestAnimationFrame(this.onFrameRequestCallback);
-		
+
 		this.update();
 		this.render();
 	}
